1d3aa124b6369fdd43d074e289f0fbb274f80b22
SuslaDmitry <67871011+SuslaDmitry@users.noreply.github.com>
2020-08-08 15:50:56 +0300

    Update settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -74,18 +74,48 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'learning_log.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dima',
-        'USER': 'dima',
-        'PASSWORD': 'Qwerty*123',
-        'HOST': '/cloudsql/upbeat-aura-285516:europe-west2:sddb',
-        'PORT': '5432',
+# [START db_setup]
+if os.getenv('GAE_APPLICATION', None):
+    # Running on production App Engine, so connect to Google Cloud SQL using
+    # the unix socket at /cloudsql/<your-cloudsql-connection string>
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'HOST': '/cloudsql/upbeat-aura-285516:europe-west2:sddb',
+            'USER': 'dima',
+            'PASSWORD': 'Qwerty*123',
+            'NAME': 'dima',
+        }
     }
-}
+else:
+    # Running locally so connect to either a local MySQL instance or connect to
+    # Cloud SQL via the proxy. To start the proxy via command line:
+    #
+    #     $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
+    #
+    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+            'NAME': 'dima',
+            'USER': 'dima',
+            'PASSWORD': 'Qwerty*123',
+        }
+    }
+# [END db_setup]
 
-
+# Use a in-memory sqlite3 database when testing in CI systems
+if os.getenv('TRAMPOLINE_CI', None):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        }
+    }
+    
+    
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 # Google App Engine: set static root for local static files
